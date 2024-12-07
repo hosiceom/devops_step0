c8b94efeedd57903be178601cd6cdd696bfe6848
hosiceom <hseum2124@gmail.com>
2024-12-07 19:30:34 +0900

    first commit

---
+++ src/main/java/kr/ac/kosmo/devops_dev0/DevopsDev0Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kosmo.devops_dev0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDev0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDev0Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/kosmo/devops_dev0/DevopsDev0ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kosmo.devops_dev0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDev0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
